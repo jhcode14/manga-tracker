4649965db1dbb085cbecda1d5f47188589836398
Jason H. <69288926+jhcode14@users.noreply.github.com>
2024-10-27 11:38:55 -0700

    add POST api for add manga + refactoring

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import List
 from flask import Flask, jsonify, request
 from db_manager import DB_Manager
 from db_definition import Manga, Episode
-from db_functions import identify_episodes
+from db_functions import identify_episodes, craw_manga_info
 from uuid import uuid4
 
 dbman = DB_Manager()
@@ -18,70 +18,83 @@ def hello_world():
 def get_manga_list():
     """API Endpoint /manga-list - response with info on all manga and it's
     episodes in DB"""
-    mangas = dbman.db.session.query(Manga).all()
-    data = []
-    for manga in mangas:
-        _, ep_l_name, ep_l_link, _, ep_c_name, ep_c_link = identify_episodes(
-            manga.episodes
-        )
+    try:
+        mangas = dbman.db.session.query(Manga).all()
+        data = []
+        for manga in mangas:
+            _, ep_l_name, ep_l_link, _, ep_c_name, ep_c_link = identify_episodes(
+                manga.episodes
+            )
 
-        data.append(
-            {
-                "name": manga.manga_name,
-                "link": manga.manga_link,
-                "episode_latest": {"name": ep_l_name, "link": ep_l_link},
-                "episode_currently_on": {"name": ep_c_name, "link": ep_c_link},
-            }
-        )
-    return jsonify(data=data, status=200, mimetype="application/json")
+            data.append(
+                {
+                    "name": manga.manga_name,
+                    "link": manga.manga_link,
+                    "episode_latest": {"name": ep_l_name, "link": ep_l_link},
+                    "episode_currently_on": {"name": ep_c_name, "link": ep_c_link},
+                }
+            )
+        return jsonify(data=data, status=200, mimetype="application/json")
+    except Exception as err:
+        return jsonify(data={"error": str(err)}, status=500)
 
 
 @server.route("/add-manga", methods=["POST"])
 def add_manga():
-    manga_id = [uuid4(), uuid4(), uuid4()]
-    episode_id = [uuid4(), uuid4(), uuid4()]
-    data = [
-        Manga(
-            manga_id=manga_id[0],
-            manga_name="一拳超人",
-            manga_link="https://m.manhuagui.com/comic/7580/",
-        ),
-        Manga(
-            manga_id=manga_id[1],
-            manga_name="每遭放逐就能获得技能的我，在100个世界大开第二轮无双",
-            manga_link="https://m.manhuagui.com/comic/50667/",
-        ),
-        Manga(
-            manga_id=manga_id[2],
-            manga_name="怪兽8号",
-            manga_link="https://m.manhuagui.com/comic/36859/",
-        ),
-        Episode(
-            episode_id=episode_id[0],
-            manga_id=manga_id[0],
-            episode_name="第247话重置版",
-            episode_link="https://m.manhuagui.com/comic/7580/772434.html",
-            episode_tag="",
-        ),
-        Episode(
-            episode_id=episode_id[1],
-            manga_id=manga_id[1],
-            episode_name="第5话",
-            episode_link="https://m.manhuagui.com/comic/50667/772823.html",
-            episode_tag="",
-        ),
-        Episode(
-            episode_id="3ACA1D34-0FAA-4DE2-A432-A6F42FC78B30",
-            manga_id=episode_id[2],
-            episode_name="第112话",
-            episode_link="https://m.manhuagui.com/comic/36859/771479.html",
-            episode_tag="",
-        ),
-    ]
-    with dbman.app.app_context():
-        print(f"adding {str(data)}")
-        for datum in data:
-            dbman.db.session.add(datum)
+    """Add manga to DB
+    Param: manga_link (str), latest (bool)
+    """
+    try:
+        # Parse POST params as JSON/Dict
+        data = request.get_json()
+
+        # Validate fields
+        if "manga_link" not in data or "latest" not in data:
+            return jsonify(
+                data={
+                    "error": "Missing required fields: 'manga_name' or 'latest'",
+                    "data": data,
+                },
+                status=400,
+            )
+
+        (
+            response_status,
+            manga_name,
+            latest_ep_name,
+            update_time,
+            crawed_ep_link,
+        ) = craw_manga_info(data["manga_link"], sleep=1)
+
+        if response_status != 200:
+            raise Exception("Unable to craw the link")
+
+        new_manga = Manga(
+            manga_id=uuid4(),
+            manga_name=manga_name,
+            manga_link=data["manga_link"],
+        )
+        new_episode1 = Episode(
+            episode_id=uuid4(),
+            manga_id=new_manga.manga_id,
+            episode_name=latest_ep_name,
+            episode_link=crawed_ep_link,
+            episode_tag="l",
+        )
+        new_episode2 = Episode(
+            episode_id=uuid4(),
+            manga_id=new_manga.manga_id,
+            episode_name=latest_ep_name if data["latest"] else "Get Started",
+            episode_link=crawed_ep_link if data["latest"] else data["manga_link"],
+            episode_tag="c",
+        )
+        with dbman.app.app_context():
+            print(f"adding {new_manga}, {new_episode1}, {new_episode2}")
+            dbman.db.session.add(new_manga)
             dbman.db.session.flush()
-        dbman.db.session.commit()
-    return f"Successfully added {data}"
+            dbman.db.session.add(new_episode1)
+            dbman.db.session.add(new_episode2)
+            dbman.db.session.commit()
+        return f"Successfully added new manga"
+    except Exception as err:
+        return jsonify(data={"error": str(err)}, status=500)
